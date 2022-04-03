abe27e080a25b152703f4d2a4cf67bade00691a6
MacBobby Chibuzor <theghostmac@gmail.com>
2022-04-03 10:29:30 +0100

    sample file.txt

--- small_hashing.go
+++ small_hashing.go
@@ -1,16 +1,14 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 )
-import (
-	crypto/sha256
-)
 
-func UsageSample()  {
+func UsageSample() {
 	fmt.Println("Usage: ", os.Args[0], " <filepath>")
 	fmt.Println("Example: ", os.Args[0], " file.txt")
 }
@@ -23,10 +21,12 @@ func checkArgs() string {
 	return os.Args[1]
 }
 
-func main()  {
+func main() {
 	filename := checkArgs()
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Printf("Sha256 value: %x\n\n", sha256.Sum256(data))
 }
