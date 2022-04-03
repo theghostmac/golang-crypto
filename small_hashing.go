ee5def6819894e2ef0a2bb31fb5ec14e9878f520
MacBobby Chibuzor <theghostmac@gmail.com>
2022-04-03 10:17:57 +0100

    small hashing

--- small_hashing.go
+++ small_hashing.go
@@ -1,1 +1,32 @@
-package golang_crypto
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+import (
+	crypto/sha256
+)
+
+func UsageSample()  {
+	fmt.Println("Usage: ", os.Args[0], " <filepath>")
+	fmt.Println("Example: ", os.Args[0], " file.txt")
+}
+
+func checkArgs() string {
+	if len(os.Args) < 2 {
+		UsageSample()
+		os.Exit(1)
+	}
+	return os.Args[1]
+}
+
+func main()  {
+	filename := checkArgs()
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
