b0d7aabdf81071a329994818307bd8091052d336
MacBobby Chibuzor <theghostmac@gmail.com>
2022-04-03 22:33:47 +0100

    exported priv and pub keys

--- retrieve_key.go
+++ retrieve_key.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+)
+
+func exportPEMStringToPrivateKey(privatekey []byte) *rsa.PrivateKey {
+	block, _ := pem.Decode(privatekey)
+	key, _ := x509.ParsePKCS1PrivateKey(block.Bytes)
+	return key
+}
+
+func exportPEMStringToPublicKey(publickey []byte) *rsa.PublicKey {
+	block, _ := pem.Decode(publickey)
+	key, _ := x509.ParsePKCS1PublicKey(block.Bytes)
+	return key
+}
+
+func readKeyFromFile(filename string) []byte {
+
+}
