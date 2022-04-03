2244cbb37eed61308bcb981ca67d1aba16068e65
MacBobby Chibuzor <theghostmac@gmail.com>
2022-04-03 21:42:28 +0100

    generated private and public key

--- generate_ppk.go
+++ generate_ppk.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"fmt"
+)
+
+func generateKeyPair(bits int) (*rsa.PrivateKey, *rsa.PublicKey) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	return privateKey, &privateKey.PublicKey
+}
+
+func main() {
+	privateKey, publicKey := generateKeyPair(2048)
+	fmt.Printf("Private Key: %v\n", privateKey)
+	fmt.Printf("Public Key: %v\n", publicKey)
+}
