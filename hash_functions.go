447382020057643993b632d0054c110e7f9621ba
MacBobby Chibuzor <theghostmac@gmail.com>
2022-04-03 12:33:03 +0100

    commit this abomination

--- hash_functions.go
+++ hash_functions.go
@@ -1,24 +1,25 @@
-package hash
+package main
 
-// Go's hashing functions and how to import them.
-
-const (
-	MD4         // import golang.org/x/crypto/md4
-	MD5         // import crypto/md5
-	SHA1        // import crypto/sha1
-	SHA224      // import crypto/sha256
-	SHA256      // import crypto/sha256
-	SHA384      // import crypto/sha512
-	SHA512      // import crypto/sha512
-	RIPEMD160   // import golang.org/x/crypto/ripemd160
-	SHA3_224    // import golang.org/x/crypto/sha3
-	SHA3_256    // import golang.org/x/crypto/sha3
-	SHA3_384    // import golang.org/x/crypto/sha3
-	SHA3_512    // import golang.org/x/crypto/sha3
-	SHA512_224  // import crypto/sha512
-	SHA512_256  // import crypto/sha512
-	BLAKE2s_256 // import golang.org/x/crypto/blake2s
-	BLAKE2b_256 // import golang.org/x/crypto/blake2b
-	BLAKE2b_384 // import golang.org/x/crypto/blake2b
-	BLAKE2b_512 // import golang.org/x/crypto/blake2b
-)
+//
+//// Go's hashing functions and how to import them.
+//
+//const (
+//	MD4         // import golang.org/x/crypto/md4
+//	MD5         // import crypto/md5
+//	SHA1        // import crypto/sha1
+//	SHA224      // import crypto/sha256
+//	SHA256      // import crypto/sha256
+//	SHA384      // import crypto/sha512
+//	SHA512      // import crypto/sha512
+//	RIPEMD160   // import golang.org/x/crypto/ripemd160
+//	SHA3_224    // import golang.org/x/crypto/sha3
+//	SHA3_256    // import golang.org/x/crypto/sha3
+//	SHA3_384    // import golang.org/x/crypto/sha3
+//	SHA3_512    // import golang.org/x/crypto/sha3
+//	SHA512_224  // import crypto/sha512
+//	SHA512_256  // import crypto/sha512
+//	BLAKE2s_256 // import golang.org/x/crypto/blake2s
+//	BLAKE2b_256 // import golang.org/x/crypto/blake2b
+//	BLAKE2b_384 // import golang.org/x/crypto/blake2b
+//	BLAKE2b_512 // import golang.org/x/crypto/blake2b
+//)
--- small_hashing.go
+++ small_hashing.go
@@ -1,32 +1,37 @@
 package main
 
 import (
-	"crypto/sha256"
-	"fmt"
-	"io/ioutil"
+	sha2562 "crypto/sha256"
+	"io"
 	"log"
 	"os"
 )
 
-func UsageSample() {
-	fmt.Println("Usage: ", os.Args[0], " <filepath>")
-	fmt.Println("Example: ", os.Args[0], " file.txt")
-}
-
-func checkArgs() string {
-	if len(os.Args) < 2 {
-		UsageSample()
-		os.Exit(1)
-	}
-	return os.Args[1]
-}
-
 func main() {
-	filename := checkArgs()
-	data, err := ioutil.ReadFile(filename)
+	file, err := os.Open("file.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer file.Close()
 
-	fmt.Printf("Sha256 value: %x\n\n", sha256.Sum256(data))
+	buffer := make([]byte, 30*1024)
+	sha256 := sha2562.New()
+	for {
+		n, err := file.Read(buffer)
+		if n > 0 {
+			_, err := sha256.Write(buffer[:n])
+			if err != nil {
+				log.Fatal()
+			}
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Printf("Read %d bytes: %v", n, err)
+			break
+		}
+	}
+	sum := sha256.Sum(nil)
+	log.Printf("%x\n", sum)
 }
